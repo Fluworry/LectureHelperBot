a26f6a498184d3fc35f4241b75fe70bbddb32252
Luent <xesfunc@riseup.net>
2021-11-30 15:13:36 +0200

    Add function to get selected days

--- inline_keyboards/tools.py
+++ inline_keyboards/tools.py
@@ -1,14 +1,28 @@
 from aiogram.types import KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def get_pressed_inline_button(inline_keyboard, callback_data, row_count=2):
-    selected_button = {}
+def get_pressed_inline_button(inline_keyboard, callback_data, row_count=2) -> dict:
+    pressed_inline_button = {}
 
     for row in range(row_count):
         current_row = tuple(dict(inline_keyboard[row]).values())
         if ('callback_data', callback_data) in current_row:
-            selected_button_index = tuple(dict(inline_keyboard[row]).values()).index(
+            pressed_button_index = tuple(dict(inline_keyboard[row]).values()).index(
                 ('callback_data', callback_data))
-            selected_button = inline_keyboard[row][selected_button_index]
+            pressed_inline_button = inline_keyboard[row][pressed_button_index]
 
-    return selected_button
+    return pressed_inline_button
+
+
+def get_selected_inline_days(inline_keyboard, row_count=2) -> list:
+    all_days = []
+    selected_days = []
+
+    for row in range(row_count):
+        all_days += list(dict(inline_keyboard[row]).keys())
+
+    for day in all_days:
+        if day[1].endswith('✅'):
+            selected_days.append(day[1].replace('✅', '').rstrip())
+
+    return selected_days
