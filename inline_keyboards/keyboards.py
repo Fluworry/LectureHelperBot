c14287255d665cffc26b9419fc352b21452e5358
Luent <xesfunc@riseup.net>
2021-12-01 19:49:39 +0200

    Change days_buttons keys to the cron format

--- inline_keyboards/keyboards.py
+++ inline_keyboards/keyboards.py
@@ -3,10 +3,10 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 choose_days_inline_keyboard = InlineKeyboardMarkup(row_width=4)
 
 days_buttons = {
-    'monday': "Пн", 'tuesday': "Вт", 'wednesday': "Ср",
-    'thursday': "Чт", 'friday': "Пт", 'saturday': "Сб",
-    'sunday': "Вс"}
-days_buttons.keys()
+    'mon': "Пн", 'tue': "Вт", 'wed': "Ср",
+    'thu': "Чт", 'fri': "Пт", 'sat': "Сб",
+    'sun': "Вс"}
+inverted_days_buttons = {day_text: day_callback for day_callback, day_text in days_buttons.items()}
 
 for btn_callback, btn_text in days_buttons.items():
     choose_days_inline_keyboard.insert(InlineKeyboardButton(btn_text, callback_data=btn_callback))
