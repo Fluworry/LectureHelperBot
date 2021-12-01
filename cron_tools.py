2dc9ef87b4bec48320f081fd49a067737028fd43
Luent <xesfunc@riseup.net>
2021-12-01 19:45:10 +0200

    Add cron tools

--- cron_tools.py
+++ cron_tools.py
@@ -2,4 +2,7 @@ from aiogram import Dispatcher
 
 
 async def lecture_notify(dp: Dispatcher, title, description, chat_id):
-    pass
+    await dp.bot.send_message(chat_id=chat_id, parse_mode='html',
+                              text=f"Уведомление о начале лекции:\n"
+                                   f"<b>{title}</b>\n\n"
+                                   f"{description}")
