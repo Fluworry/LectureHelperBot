8c9d3c4859c619ce5696637dcbd8f54882ba6297
Luent <xesfunc@riseup.net>
2021-12-02 20:18:05 +0200

    Add CronJob model

--- db_models.py
+++ db_models.py
@@ -1,5 +1,6 @@
+import peewee
 from peewee import *
-from inline_keyboards.keyboards import days_buttons
+from inline_keyboards.days_keyboard import days_buttons
 
 
 db = SqliteDatabase('lecture_bot.db')
@@ -20,18 +21,24 @@ class Course(BaseModel):
 #     course = ForeignKeyField(Course)
 
 
+class CronJob(BaseModel):
+    job_id = CharField()
+
+
 class Day(BaseModel):
     weekday = CharField()
 
 
 class Lecture(BaseModel):
-    lecture_name = CharField(50)
+    lecture_name = CharField(50, unique=True)
     description = CharField(100, null=True)
     days = ManyToManyField(Day, backref='lectures')
+    cron_jobs = ManyToManyField(CronJob, backref='lectures')
     course = ForeignKeyField(Course)
 
 
 LectureDay = Lecture.days.get_through_model()
+LectureCronJob = Lecture.cron_jobs.get_through_model()
 
 # class LectureToDay(BaseModel):
 #     lecture = ForeignKeyField(Lecture)
@@ -42,9 +49,11 @@ LectureDay = Lecture.days.get_through_model()
 def db_init():
     db.create_tables([
         Course,
+        CronJob,
         Day,
         Lecture,
-        LectureDay
+        LectureDay,
+        LectureCronJob
     ])
 
     for day in days_buttons.values():
