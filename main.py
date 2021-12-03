ff80203d855f77178feace51cf1833d38b8375bd
Luent <xesfunc@riseup.net>
2021-12-03 19:35:08 +0200

    Add \n to notify message

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ scheduler = AsyncIOScheduler(config)
 
 async def lecture_notify(title, description, chat_id):
     await bot.send_message(chat_id=chat_id, parse_mode='html',
-                           text=f"Уведомление о начале лекции:\n"
+                           text=f"Уведомление о начале лекции:\n\n\n"
                                 f"<b>{title}</b>\n\n"
                                 f"{description}")
 
