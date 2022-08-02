b530c1b990d7f271f11f7db04d736ea09c99b591
citannu <xesfunc@riseup.net>
2022-08-02 13:46:27 +0300

    Change models to sqlalchemy

---
+++ db/models.py
@@ -0,0 +1,92 @@
+from sqlalchemy import Column, String, Integer, BigInteger, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+from loader import DB_USER, DB_PASS, DB_HOST, DB_NAME
+
+from keyboards.inline import days_buttons
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger)
+    group_id = Column(Integer, ForeignKey("group.id"))
+
+
+lecture_cronjob_table = Table(
+    "lecture_cronjob",
+    Base.metadata,
+    Column("lecture_id", ForeignKey("lecture.id"), primary_key=True),
+    Column("cronjob_id", ForeignKey("cronjob.id"), primary_key=True)
+)
+
+lecture_weekday_table = Table(
+    "lecture_weekday",
+    Base.metadata,
+    Column("lecture_id", ForeignKey("lecture.id"), primary_key=True),
+    Column("weekday_id", ForeignKey("weekday.id"), primary_key=True)
+)
+
+
+class Group(Base):
+    __tablename__ = "group"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    invite_token = Column(String(15), unique=True)
+    lecture = relationship("Lecture")
+    users = relationship("User")
+
+
+class WeekDay(Base):
+    __tablename__ = "weekday"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    lectures = relationship(
+        "Lecture", secondary=lecture_weekday_table, back_populates="weekday"
+    )
+
+
+class CronJob(Base):
+    __tablename__ = "cronjob"
+
+    id = Column(Integer, primary_key=True)
+    job_id = Column(String(255))
+
+
+class Lecture(Base):
+    __tablename__ = "lecture"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    description = Column(String(50), nullable=True)
+
+    group_id = Column(Integer, ForeignKey("group.id"))
+    weekday = relationship(
+        "WeekDay", secondary=lecture_weekday_table, back_populates="lectures"
+    )
+    cronjob = relationship("CronJob", secondary=lecture_cronjob_table)
+
+
+def db_init():
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}", 
+        future=True, echo=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for day in days_buttons.values():
+        session.add(WeekDay(name=day))
+    
+    session.commit()
+    session.close()
