960195bd60859bf701c3559e937bcfa454db5188
citannu <xesfunc@riseup.net>
2022-08-05 21:53:22 +0300

    Connect to postgres db using sqlalchemy

--- loader.py
+++ loader.py
@@ -4,17 +4,38 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from cron import cron_config
 
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
 from dotenv import load_dotenv
 import os
 import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
 
-logging.basicConfig(level=logging.INFO)
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+# Load some additional env variables
+SOURCE_CODE_LINK = os.getenv("SOURCE_CODE_LINK")
+
+engine = create_async_engine(
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}",
+    future=True
+)
+async_sessionmaker = sessionmaker(
+    engine, class_=AsyncSession
+)
 
 bot = Bot(token=API_TOKEN)
+bot["db"] = async_sessionmaker
 dp = Dispatcher(bot, storage=MemoryStorage())
 scheduler = AsyncIOScheduler(cron_config.config)
 
