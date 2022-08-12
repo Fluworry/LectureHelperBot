962d5e0f3a29297996afdfd6356de915d808d06d
citannu <xesfunc@riseup.net>
2022-08-12 16:01:30 +0300

    Add state to manage and leave a group

--- states/lecture.py
+++ states/lecture.py
@@ -3,10 +3,12 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class LectureStates(StatesGroup):
     normal = State()
-    waiting_for_title = State()
+    waiting_for_name = State()
     waiting_for_description = State()
     waiting_for_day = State()
     waiting_for_time = State()
 
-    lecture_edit_choose = State()
+    leave_group = State()
+    manage_own_group = State()
+
     lecture_edit = State()
